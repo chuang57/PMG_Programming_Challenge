01cfbbe285f9d8e17b8811ecf0dbbd0204067e13
chuang57 <christiandhuang7@gmail.com>
2023-02-05 16:36:06 -0500

    ci

--- csv-combiner/test_csv_combiner.py
+++ csv-combiner/test_csv_combiner.py
@@ -8,7 +8,7 @@ class TestCSVCombiner(unittest.TestCase):
     def test_two_files(self):
         """test combination of two csv files"""
         result = subprocess.run(['python3', 'csv_combiner.py', './test-fixtures/accessories.csv',
-                                './test-fixtures/clothing.csv'], stdout=subprocess.PIPE)
+                                './test-fixtures/clothing.csv'], stdout=subprocess.PIPE, universal_newlines='\n')
         expected_output = b'email_hash,category,filename\r\nb9f6f22276c919da793da65c76345ebb0b072257d12402107d09c89bc369a6b6,Wallets,accessories.csv\r\nc2b5fa9e09ef2464a2b9ed7e351a5e1499823083c057913c6995fdf4335c73e7,Purses,accessories.csv\r\nfaaee0ff7d06b05313ecb75d46a9aed014b11023ca1af5ec21a0607848071d18,Watches,accessories.csv\r\n5cd72da5035f2b36b604a16efc639cd04b6cfae7e487dcba60db88d3ef870f1e,Wallets,accessories.csv\r\nb9f6f22276c919da793da65c76345ebb0b072257d12402107d09c89bc369a6b6,Cardigans,clothing.csv\r\nc2b5fa9e09ef2464a2b9ed7e351a5e1499823083c057913c6995fdf4335c73e7,Cardigans,clothing.csv\r\nfaaee0ff7d06b05313ecb75d46a9aed014b11023ca1af5ec21a0607848071d18,Tanks,clothing.csv\r\n5cd72da5035f2b36b604a16efc639cd04b6cfae7e487dcba60db88d3ef870f1e,Tanks,clothing.csv'
         self.assertEqual(result.stdout.strip(), expected_output,
                          "CSV files not combined correctly")
